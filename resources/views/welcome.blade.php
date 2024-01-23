a5ab0ec0d5719c137af0844d6e03b867ca405d9a
salvatore <totoilprincipe47@gmail.com>
2024-01-23 19:21:19 +0000

    nuovo progetto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="welcome.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt animi maiores porro aperiam sunt, soluta sint eligendi beatae magnam voluptatum est accusamus odit consequatur sapiente nemo quidem. Quis, hic vel!</p>
+    <div class="ciao"></div>
+</body>
+</html>
